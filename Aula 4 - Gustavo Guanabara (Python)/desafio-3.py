ce0235495266dd9a8b6eeaa776c81516b4e39d28
Daniel N <80979307+odanilas@users.noreply.github.com>
2022-04-11 12:06:50 -0400

    Aula 4 - Gustavo Guanabara (Python)

---
+++ Aula 4 - Gustavo Guanabara (Python)/desafio-3.py
@@ -0,0 +1,7 @@
+print('==== DESAFIO 03 ====')
+pnumero = input('Primeiro número: ')
+pnumero = int(pnumero)
+snumero = input('Segundo número: ')
+snumero = int(snumero)
+
+print('A soma é: ', pnumero + snumero)
